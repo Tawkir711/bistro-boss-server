939f4f6377153415d20aa68a3d45eb6124a9a362
Tawkir711 <mdtawkirk711@gmail.com>
2023-11-29 12:21:02 +0600

    added all code

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -69,6 +70,24 @@ async function run() {
       }
       next();
     }
+
+
+
+    // payment system
+    app.post('/create-payment-intent', async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      });
+    })
 
 
     // users collection
